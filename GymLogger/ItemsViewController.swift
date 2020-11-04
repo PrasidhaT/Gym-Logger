6a600d7e625973fcfdaff5a906f1f4469635c172
Prasidha Timsina <prasidha.timsina@uvm.edu>
2020-11-04 13:35:40 -0500

    finished chapter 11

--- GymLogger/ItemsViewController.swift
+++ GymLogger/ItemsViewController.swift
@@ -44,9 +44,7 @@ class ItemsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Get the height of the status bar
         let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
@@ -59,9 +57,8 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView,
         cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            // Create an instance of UITableViewCell, with default appearance
+            // Creating an instance of UITableViewCell
             let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-            
             // Set the text on the cell with the description of the item
             // that is at the nth index of items, where n = row this cell
             // will appear in on the tableview
@@ -75,21 +72,41 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView,
                             commit editingStyle: UITableViewCell.EditingStyle,
-                            forRowAt indexPath: IndexPath) {
-        // If the table view is asking to commit a delete command...
-        if editingStyle == .delete {
-            let item = itemStore.allItems[indexPath.row]
-            // Remove the item from the store
-            itemStore.removeItem(item)
-            // Also remove that row from the table view with an animation
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-        }
+        forRowAt indexPath: IndexPath) {
+            // If the table view is asking to commit a delete command...
+            if editingStyle == .delete {
+                let item = itemStore.allItems[indexPath.row]
+                
+                
+                let title = "Delete \(item.name)?"
+                let message = "Are you sure you want to delete this exercise?"
+                
+                let ac = UIAlertController(title: title,
+                    message: message,
+                    preferredStyle: .actionSheet)
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                ac.addAction(cancelAction)
+                
+                let deleteAction = UIAlertAction(title: "Delete", style: .destructive,
+                    handler: { (action) -> Void in
+                        // Remove the item from the store
+                        self.itemStore.removeItem(item)
+                        
+                        // Also remove that row from the table view with an animation
+                        self.tableView.deleteRows(at: [indexPath], with: .automatic)
+                })
+                ac.addAction(deleteAction)
+                
+                // Present the alert controller
+                present(ac, animated: true, completion: nil)
+            }
     }
     
     override func tableView(_ tableView: UITableView,
     moveRowAt sourceIndexPath: IndexPath,
     to destinationIndexPath: IndexPath) {
-         // Update the model
+         // Update model
          itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
     }
 
